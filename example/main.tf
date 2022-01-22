fba1ed91b7c3d8f190b2e685676617e403789eb5
mehulsharma2010 <75750488+mehulsharma2010@users.noreply.github.com>
2022-01-22 19:04:02 +0530

    Adding VM code for security check

--- example/main.tf
+++ example/main.tf
@@ -34,3 +34,57 @@ module "vnet" {
     Owner = "Akansha"
   }
 }
+    
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = module.res_group.resource_group_name
+  virtual_network_name = module.vnet.vnet_name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+   
+
+resource "azurerm_network_interface" "main" {
+  name                = "my-nic"
+  location            = module.res_group.resource_group_location
+  resource_group_name = module.res_group.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "my-vm"
+  location              = module.res_group.resource_group_location
+  resource_group_name   = module.res_group.resource_group_name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
