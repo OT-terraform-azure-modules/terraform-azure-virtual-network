24ef574216fc7f078d1e0b33bc173c51dbca457b
AkankshaS15 <48242322+AkankshaS15@users.noreply.github.com>
2022-01-22 15:56:09 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -35,31 +35,4 @@ module "vnet" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "bad_linux_example" {
-  name                            = "bad-linux-machine"
-  resource_group_name             = azurerm_resource_group.example.name
-  location                        = azurerm_resource_group.example.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
-  admin_password                  = "somePassword"
-  disable_password_authentication = false
-}
 
-resource "azurerm_virtual_machine" "bad_example" {
-    name                            = "bad-linux-machine"
-    resource_group_name             = azurerm_resource_group.example.name
-    location                        = azurerm_resource_group.example.location
-    size                            = "Standard_F2"
-    admin_username                  = "adminuser"
-    admin_password                  = "somePassword"
-
-    os_profile {
-        computer_name  = "hostname"
-        admin_username = "testadmin"
-        admin_password = "Password1234!"
-    }
-
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
-  }
